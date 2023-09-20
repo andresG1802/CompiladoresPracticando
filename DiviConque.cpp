e0dc9b2bdd3746d7481a5818bc4d7e6728eb4909
andre <andresdaniel.go2020@gmail.com>
2023-09-20 01:26:26 -0500

    Agregando nuevas Implementaciones

--- DiviConque.cpp
+++ DiviConque.cpp
@@ -7,6 +7,7 @@ using namespace std;
 vector<string> cadenasDividas;
 string parte;
 string cadenaSinAsterisco;
+
 void DivideConquistaras(string cadena)
 {
     istringstream stream(cadena);
@@ -15,7 +16,6 @@ void DivideConquistaras(string cadena)
     {
         if(getline(stream,parte,'*'))
         {
-            
             cadenasDividas.push_back(parte+'*');
         }
         if(!getline(stream,parte,'*'))
@@ -37,6 +37,34 @@ void DivideConquistaras(string cadena)
         }
     }   
 }
+void verificandoAsteriscoEnlaCadena(string cadena)
+{
+    bool tieneAsterisco=false;
+    int contadorAsterisco=0;
+    for(char caracter:cadena)
+    {
+        if(caracter=='*')
+        {
+            tieneAsterisco=true;
+            contadorAsterisco++;
+        }   
+    }
+    //En caso que la cadena sea : ab*
+    if(cadena[cadena.size()-1]=='*' && contadorAsterisco==1)
+    {
+        cadenasDividas.push_back(cadena);
+    }
+    else if(tieneAsterisco)
+    {
+        //En caso tenga asterisco en el medio por ejemplo a*b* o a*b
+        DivideConquistaras(cadena);
+    }
+    else
+    {
+        //En caso se una expresion sin Asterisco
+        cadenasDividas.push_back(cadena);
+    }
+}
 vector<string> divideSubCadena(string cadena)
 {
     vector<string> vectorCadenas;
@@ -55,58 +83,74 @@ vector<string> divideSubCadena(string cadena)
     
     return vectorCadenas;
 }
+vector<string> separarO(string cadena)
+{
+    vector<string> elementos;
+    string elemento;
 
+    istringstream stream(cadena);
 
+    while(getline(stream,elemento,'|'))
+    {
+        elementos.push_back(elemento);
+    }
+
+    return elementos;
+}
+
+//Regla 1:Si es un cadenaSimple
+//Regla 2: Si termina en asterisco ab*
+//Regla 3: Si tiene varios o tiene un asterisco al final
 int main()
 {
     string cadena;
     int cantidad;
 
-    bool tieneAsterisco=false;
-    int contadorAsterisco=0;
+    vector<string> alfabetoGenerado;
     
     cout<<"Cadena: ";cin>>cadena;
     cout<<"Cantidad: ";cin>>cantidad;
     
-    for(char caracter:cadena)
+    //El primer for no hace mas que iterar nuestra logica craneada
+    int contador=0;
+
+    //Separar por categorias
+
+    for(string cadena:separarO(cadena))
     {
-        if(caracter=='*')
+        verificandoAsteriscoEnlaCadena(cadena);
+
+
+        cout<<contador+1<<".Imprimiendo cadena divida:"<<endl;
+        //Hacer un array bidimensional
+
+        vector<vector<string>> subCadenasSeparadas;
+        vector<string> vectorSubCadena;
+        
+        for(string& subCadena:cadenasDividas)
         {
-            tieneAsterisco=true;
-            contadorAsterisco++;
-        }   
-    }
-    if(cadena[cadena.size()-1]=='*' && contadorAsterisco==1)
-    {
-        cadenasDividas.push_back(cadena);
-    }
-    else if(tieneAsterisco)
-    {
-        DivideConquistaras(cadena);
-    }
-    else
-    {
-        cadenasDividas.push_back(cadena);
+            cout<<subCadena<<endl;
+            if(subCadena.size()>2)
+            {
+                vectorSubCadena = divideSubCadena(subCadena);
+                subCadenasSeparadas.push_back(vectorSubCadena);
+            }
+            else{
+                
+            }
+        }
+        cout<<"Imprimiendo subCadenas de una cadena ya dividida"<<endl;
+        for(int i=0;i<subCadenasSeparadas.size();++i)
+        {
+            for(int j=0;j<subCadenasSeparadas[0].size();++j)
+            {
+                cout<<subCadenasSeparadas[i][j]<<endl;
+            }
+        }
+
+        contador++;
     }
 
-    cout<<"Imprimiendo cadena divida:"<<endl;
-    //Hacer un array bidimensional
-    vector<string> vectorSubCadena;
-    for(string& subCadena:cadenasDividas)
-    {
-        cout<<subCadena<<endl;
-        if(subCadena.size()>2)
-        {
-            vectorSubCadena=divideSubCadena(subCadena);
-        }
-        else{
-
-        }
-    }
-    cout<<"Imprimiendo subCadena de una cadena dividida"<<endl;
-    for(string& cadena:vectorSubCadena)
-    {
-        cout<<cadena<<endl;
-    }
+    
     return 0;
 }
