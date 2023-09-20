91f339e0bb2d6b9a54acb6912b1e69843fad55b9
andre <andresdaniel.go2020@gmail.com>
2023-09-20 04:46:03 -0500

    agregandoo mas reglas

--- DiviConque.cpp
+++ DiviConque.cpp
@@ -132,17 +132,45 @@ bool regla2(string cadena)
         }
     }
 
-    if(contador==1 && cadena[cadena.size()-1]=='*'){
+    if(contador==1 && cadena[cadena.size()-1]=='*'&&cadena.size()>2){
         cumpleRegla=true;
     }
 
     return cumpleRegla;
 }
-//Regla 3: Si tiene varios o tiene un asterisco al final
+//Regla 3: para a*
+bool regla3(string cadena)
+{
+    bool cumpleRegla=false;
+    if(cadena.size() == 2 && cadena[cadena.size()-1]=='*')
+    {
+        cumpleRegla=true;
+    }
+    return cumpleRegla;
+}
+//Regla 4: para a*b
+bool regla4(string cadena)
+{
+    bool cumpleRegla=false;
+    if(cadena.size() == 3 && cadena[cadena.size()-2]=='*')
+    {
+        cumpleRegla=true;
+    }
+    return cumpleRegla;
+}
+//Regla 5: para a*b*
+bool regla5(string cadena)
+{
+    bool cumpleRegla=false;
+    if(cadena.size() == 4 && cadena[1]=='*' && cadena[cadena.size()-1]=='*')
+    {
+        cumpleRegla=true;
+    }
+    return cumpleRegla;
+}
 
 
-
-vector<string> funcionParaCadenaAsteriscoAlFinal(string cadena,int cantidad)
+vector<string> funcionParaCadenaAsteriscoAlFinalRegla2(string cadena,int cantidad)
 {
     vector<string> cadenasGeneradas;
     string parteSinasterisco;
@@ -162,11 +190,68 @@ vector<string> funcionParaCadenaAsteriscoAlFinal(string cadena,int cantidad)
 
     return cadenasGeneradas;
 }
+vector<string> funcionRegla3(string cadena, int cantidad)
+{
+    vector<string> cadenasGeneradas;
+    string generadorCaracter;
+
+    cadenasGeneradas.push_back(" ");
+    for(int i=0;i<cantidad;i++)
+    {
+        generadorCaracter+=cadena[0];
+        cadenasGeneradas.push_back(generadorCaracter);
+    }
+
+    return cadenasGeneradas;
+}
+
+vector<string> funcionRegla4(string cadena,int cantidad)
+{
+    vector<string> cadenasGeneradas;
+    string parteSinAsterisco; 
+    string generadorCaracter;
+    parteSinAsterisco+= cadena[cadena.size()-1];
+    cadenasGeneradas.push_back(parteSinAsterisco);
+
+    for(int i=0;i<cantidad;i++)
+    {
+        generadorCaracter+=cadena[0];
+        cadenasGeneradas.push_back(generadorCaracter+parteSinAsterisco);
+    }
+}
+vector<string> funcionRegla5(string cadena,int cantidad)
+{
+    vector<string> cadenasGeneradas;
+    string caracter1;
+    caracter1+=cadena[0];
+    string caracter2;
+    caracter2+=cadena[cadena.size()-1];
+    string generadorCaracter1;
+    string generadorCaracter2;
+
+    cadenasGeneradas.push_back(" ");
+    cadenasGeneradas.push_back(caracter1);
+    cadenasGeneradas.push_back(caracter2);
+    for(int i=0;i<cantidad;i++)
+    {
+        
+        for(int j=0;i<cantidad;j++)
+        {
+
+        }
+    }
+
+    return cadenasGeneradas;
+}
+
+
 
 int main()
 {
     string cadena;
     int cantidad;
+    vector<vector<string>> alfabeto4;
+    vector<string> alfabeto3;
     vector<vector<string>> alfabetoGenerado2;
     vector<string> alfabetoGenerado1;
     
@@ -186,40 +271,53 @@ int main()
         }
         else if(regla2(cadena))
         {
-            vector<string> cadenasIteradasParaRegla2 = funcionParaCadenaAsteriscoAlFinal(cadena,cantidad);
+            vector<string> cadenasIteradasParaRegla2 = funcionParaCadenaAsteriscoAlFinalRegla2(cadena,cantidad);
             alfabetoGenerado2.push_back(cadenasIteradasParaRegla2);
         }
-        else if(!regla1(cadena) && !regla2(cadena))
+        else if(regla3(cadena))
         {
-            verificandoAsteriscoEnlaCadena(cadena);
-            cout<<contador+1<<".Imprimiendo cadena divida:"<<endl;
-            //Hacer un array bidimensional
+            // verificandoAsteriscoEnlaCadena(cadena);
+            // cout<<contador+1<<".Imprimiendo cadena divida:"<<endl;
+            // //Hacer un array bidimensional
 
-            vector<vector<string>> subCadenasSeparadas;
-            vector<string> vectorSubCadena;
+            // vector<vector<string>> subCadenasSeparadas;
+            // vector<string> vectorSubCadena;
             
-            for(string& subCadena:cadenasDividas)
-            {
-                cout<<subCadena<<endl;
-                if(subCadena.size()>2)
-                {
-                    vectorSubCadena = divideSubCadena(subCadena);
-                    subCadenasSeparadas.push_back(vectorSubCadena);
-                }
-                else{
+            // for(string& subCadena:cadenasDividas)
+            // {
+            //     cout<<subCadena<<endl;
+            //     if(subCadena.size()>2)
+            //     {
+            //         vectorSubCadena = divideSubCadena(subCadena);
+            //         subCadenasSeparadas.push_back(vectorSubCadena);
+            //     }
+            //     else{
                     
-                }
-            }
-            cout<<"Imprimiendo subCadenas de una cadena ya dividida"<<endl;
-            for(int i=0;i<subCadenasSeparadas.size();++i)
-            {
-                for(int j=0;j<subCadenasSeparadas[0].size();++j)
-                {
-                    cout<<subCadenasSeparadas[i][j]<<endl;
-                }
-            }
-            contador++;
+            //     }
+            // }
+            // cout<<"Imprimiendo subCadenas de una cadena ya dividida"<<endl;
+            // for(int i=0;i<subCadenasSeparadas.size();++i)
+            // {
+            //     for(int j=0;j<subCadenasSeparadas[0].size();++j)
+            //     {
+            //         cout<<subCadenasSeparadas[i][j]<<endl;
+            //     }
+            // }
+            // contador++;
+
+            alfabeto3 = funcionRegla3(cadena,cantidad);
+
         }
+        else if(regla4(cadena))
+        {
+            vector<string> cadenasIteradasParaRegla4 =funcionRegla4(cadena,cantidad);
+            alfabeto4.push_back(cadenasIteradasParaRegla4);
+        }
+        else if(regla5(cadena))
+        {
+
+        }
+
     }
 
     
@@ -234,6 +332,20 @@ int main()
             for(int j=0;j<alfabetoGenerado2[i].size();++j)
             {
                 AlfabetoFinal.push_back(alfabetoGenerado2[i][j]);
+            }
+        }
+    }
+    for(int i=0;i<alfabeto3.size();i++)
+    {
+        AlfabetoFinal.push_back(alfabeto3[i]);
+    }
+    if(alfabeto4.size()>0)
+    {
+        for(int i=0;i<alfabeto4.size();++i)
+        {
+            for(int j=0;j<alfabeto4[i].size();++j)
+            {
+                AlfabetoFinal.push_back(alfabeto4[i][j]);
             }
         }
     }
